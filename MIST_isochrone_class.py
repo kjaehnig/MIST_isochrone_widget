0720a7f0aacc5b3dab98fa9aefe7308cdc8c670d
kjaehnig <kjaehnig@yahoo.com>
2021-09-06 16:43:39 -0400

    added MIST python class file

--- MIST_isochrone_class.py
+++ MIST_isochrone_class.py
@@ -1,12 +1,7 @@
 
 
-
-
-
-
 class MIST_isochrone_class():
-    """Base class for generating isochrones using 
-    ISOCHRONES package
+    """Base class for generating isochrones
     """
     def __init__(self):
         self.num_stars = 1000
@@ -16,19 +11,7 @@ class MIST_isochrone_class():
         self.has_masses = False
         self.has_tracks = False
 
-        self.iso_bounds = {
-            'age': [5.1, 12.5], 
-            'av' : [0.0, 6.0], ## Hard Limit, will not crash
-            'dist' : [1.0, 30000],  ## Don't go below 1.0parsecs
-            'feh' : [-4, 0.49] ## going out of feh lims will crash python
-        }
-
-    def generate_mass(self, N=None, IMF='chabrier'):
-        """
-        Generate an initial IMF distribution of masses
-        """
-        if N is None :
-            N=self.num_stars
+    def generate_mass(self, N, IMF='chabrier'):
         if IMF is 'chabrier':
             from isochrones.priors import ChabrierPrior as iso_IMF
             self.IMF='chabrier'
@@ -40,12 +23,9 @@ class MIST_isochrone_class():
         self.has_masses = True
 
 
-    def get_tracks(self, TRACKS=False, BASIC=False):
+    def get_tracks(self):
         from isochrones import get_ichrone
-        """
-        Get evolutionary tracks to generate isochrones
-        """
-        tracks = get_ichrone('mist', tracks=TRACKS, basic=BASIC)
+        tracks = get_ichrone('mist', tracks=False, basic=False)
         self.has_tracks = True
         self.tracks = tracks
 
@@ -54,18 +34,12 @@ class MIST_isochrone_class():
                             age=7.0, 
                             av=1.0, 
                             dist=10.0, 
-                            feh=0.00):
-        """
-        Generate synthetic photometry for isochrone
-        """
+                            feh=0.02):
         if self.has_masses is False:
-            self.generate_mass()
+            self.generate_mass
         if self.has_tracks is False:
-            self.get_tracks()
+            self.get_tracks
 
-        if feh < -4 or feh > 0.49:
-            raise Exception("GOING OUTSIDE OF FeH LIMITS WILL CRASH PYTHON")
-            return
         iso_df = self.tracks.generate(self.masses,
                                     age,
                                     feh,
